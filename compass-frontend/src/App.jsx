71b3ee707396d58e1636e6686526f2036288dc94
LasimaP <lasimap@gmail.com>
2026-07-15 10:20:32 -0400

    added tailwindcss to project

--- compass-frontend/src/App.jsx
+++ compass-frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import ListHabitComponent from "./components/ListHabitComponent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HabitComponent from "./components/HabitComponent";
