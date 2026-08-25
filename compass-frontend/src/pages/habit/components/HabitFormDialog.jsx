cbcea9e201dd4d251f2c5e4b213cbb4435463a48
LasimaP <lasimap@gmail.com>
2026-08-25 11:08:53 -0400

    feat: add day toggle buttons to dialog box

--- compass-frontend/src/pages/habit/components/HabitFormDialog.jsx
+++ compass-frontend/src/pages/habit/components/HabitFormDialog.jsx
@@ -3,15 +3,40 @@ import {
   Dialog,
   DialogPanel,
   DialogTitle,
+  Fieldset,
+  Field,
+  Label,
+  Input,
+  Switch,
+  RadioGroup,
+  Radio,
 } from "@headlessui/react";
-import React from "react";
+import React, { useState } from "react";
+import { DayOfWeek, DayOfWeekOptions, Frequency } from "../constants/cadence";
 
 const HabitFormDialog = ({ habit, isOpen, onClose }) => {
   const isEdit = habit !== null;
+  const [frequency, setFrequency] = useState(Frequency.DAILY);
+  const [activeDays, setActiveDays] = useState([]);
+
+  const dayButtonStyle = (day) => {
+    return `w-12 h-12 font-body font-semibold border border-bark/10 rounded-xl transition duration-150 hover:cursor-pointer hover:border-sage ${activeDays.includes(day) ? "bg-sage text-white" : "bg-white text-bark/80"}`;
+  };
+
+  const toggleDay = (day) => {
+    if (!activeDays.includes(day)) {
+      setActiveDays((prevDays) => [...prevDays, day]);
+      console.log("added " + day);
+    } else {
+      setActiveDays((prevDays) => prevDays.filter((d) => d !== day));
+      console.log("removed " + day);
+    }
+  };
+
   return (
     <Dialog open={isOpen} onClose={onClose} className="relative z-50">
       <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-        <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
+        <DialogPanel className="max-w-lg space-y-4 border bg-linen p-12">
           <DialogTitle>
             {isEdit ? "Edit habit" : "Plant a new habit"}
           </DialogTitle>
@@ -24,6 +49,56 @@ const HabitFormDialog = ({ habit, isOpen, onClose }) => {
             <button onClick={onClose}>Cancel</button>
             <button>{isEdit ? "Save changes" : "Plant habit"}</button>
           </div>
+          <Fieldset>
+            <Field>
+              <Label className="block uppercase">Name</Label>
+              <Input className="mt-1 bg-white" name="name"></Input>
+            </Field>
+            <Field>
+              <Label className="block uppercase">
+                Description <span className="lowercase pl-6">optional</span>
+              </Label>
+              <Input className="mt-1 bg-white" name="description"></Input>
+            </Field>
+            <Field>
+              <Label className="block uppercase">Cadence</Label>
+              <RadioGroup
+                name="frequency"
+                value={frequency}
+                onChange={setFrequency}
+                aria-label="Cadence"
+              >
+                <div className="flex justify-center gap-px bg-bark/10 border border-bark/10 rounded-xl overflow-hidden cursor-pointer">
+                  <Radio
+                    value={Frequency.DAILY}
+                    className="w-[100%] bg-linen text-center data-checked:bg-sage"
+                  >
+                    <p>Daily</p>
+                  </Radio>
+                  <Radio
+                    value={Frequency.SPECIFIC_DAYS}
+                    className="w-[100%] bg-linen text-center data-checked:bg-sage"
+                  >
+                    <p>Specific Days</p>
+                  </Radio>
+                </div>
+              </RadioGroup>
+              {frequency === Frequency.SPECIFIC_DAYS && (
+                <div className="flex gap-2">
+                  {DayOfWeekOptions.map((day) => (
+                    <button
+                      key={day.value}
+                      onClick={() => toggleDay(day.value)}
+                      className={dayButtonStyle(day.value)}
+                      aria-pressed={activeDays.includes(day.label)}
+                    >
+                      {day.label}
+                    </button>
+                  ))}
+                </div>
+              )}
+            </Field>
+          </Fieldset>
         </DialogPanel>
       </div>
     </Dialog>
--- compass-frontend/src/pages/habit/constants/cadence.js
+++ compass-frontend/src/pages/habit/constants/cadence.js
@@ -1,6 +1,6 @@
 export const Frequency = {
   DAILY: "DAILY",
-  SPECIFIC_DAYS: "SPECIFIC_DAYS",
+  SPECIFIC_DAYS: "SPECIFIC DAYS",
 };
 
 export const DayOfWeek = {
@@ -12,3 +12,13 @@ export const DayOfWeek = {
   SATURDAY: "SATURDAY",
   SUNDAY: "SUNDAY",
 };
+
+export const DayOfWeekOptions = [
+  { value: DayOfWeek.MONDAY, label: "M" },
+  { value: DayOfWeek.TUESDAY, label: "T" },
+  { value: DayOfWeek.WEDNESDAY, label: "W" },
+  { value: DayOfWeek.THURSDAY, label: "T" },
+  { value: DayOfWeek.FRIDAY, label: "F" },
+  { value: DayOfWeek.SATURDAY, label: "S" },
+  { value: DayOfWeek.SUNDAY, label: "S" },
+];
